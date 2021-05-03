ef9c399bcc9cfbbff66cd10cd3fb10fef87c922b
BritishPandaChick <sbartley1080@gmail.com>
2021-05-03 10:58:21 -0400

    Made changes to _form.html.erb and edit.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if @article.sav
+    if @article.save
       redirect_to @article
     else
       render :new
